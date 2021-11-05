f570f7ee3409273e5a79b3a4ac748f828ebc493f
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-05 19:52:51 -0300

    Add files via upload

---
+++ EXTRA03 - sum.py
@@ -0,0 +1,18 @@
+end = int(input("Enter how many numbers will be summed:"))
+sum = 0
+number = 0
+count = 0
+ 
+while count < end:
+    number = int(input("Enter a number to be summed: "))
+    sum = sum + number
+    count = count + 1
+
+print("The sum is:", sum)
+
+
+
+
+    
+
+
