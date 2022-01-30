5b236d78206da0b1ec87ff653f06b3bd214e1b8a
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2022-01-30 14:36:24 -0300

    Update EXTRA03 - sum.py

--- EXTRA03 - sum.py
+++ EXTRA03 - sum.py
@@ -1,9 +1,9 @@
-end = int(input("Enter how many numbers will be summed:"))
+last_number = int(input("Enter how many numbers will be summed:"))
 sum = 0
 number = 0
 count = 0
  
-while count < end:
+while count < last_number:
     number = int(input("Enter a number to be summed: "))
     sum = sum + number
     count = count + 1
